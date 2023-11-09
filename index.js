c442864495a7c2817c2945346b9aa5b20b14a8c3
mamunhero <96989895+mamunhero@users.noreply.github.com>
2023-11-09 15:33:46 +0600

    get for room detailes

--- index.js
+++ index.js
@@ -57,6 +57,8 @@ async function run() {
       res.send(result)
       console.log("addroomid", result);
     })
+
+    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
